a29a0de31052b08e748af2f4fa83e0fcf27df755
Valentin Levalet <valentin.levalet@live.fr>
2019-02-07 22:22:41 +0100

    Thumbnails + lightbox

--- index.php
+++ index.php
@@ -4,17 +4,40 @@
     <title>invader map</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="styles/leaflet.css" />
-    <script src="scripts/leaflet.js"></script>
+    <script src="scripts/leaflet-src.js"></script>
     <style type="text/css">
-        #mapid {
-            width: 400px;
-            height: 400px;
+        html, body, #mapid {
+            width: 100%;
+            height: 100%;
+        }
+        body {
+            margin: 0;
+        }
+        .leaflet-marker-icon {
+            height: 40px;
+            width: 40px;
+            border: solid 2px white;
+            background: white;
+            box-shadow: -2px 4px 4px rgba(0,0,0,.25);
+            border-radius: 4px;
+            transition: all .15s ease;
+        }
+        .leaflet-marker-icon:hover{
+            height: 80px;
+            width: 80px;
         }
     </style>
+
+    <script type="text/javascript">
+    shutterOnload = function(){shutterReloaded.Init();}
+    </script>
+    <script src="scripts/shutter.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="styles/shutter.css" type="text/css" media="screen" />
 </head>
 	<body>
 <?php
 
+// convert Exif GPS array https://stackoverflow.com/a/16437888/4264406
 function gps($coordinate, $hemisphere) {
   if (is_string($coordinate)) {
     $coordinate = array_map("trim", explode(",", $coordinate));
@@ -35,19 +58,27 @@ function gps($coordinate, $hemisphere) {
 }
 
 $files = scandir('images/');
-foreach ($files as $file) {
 
-    if($file != '.' && $file != '..'){
+// ignorer dossier thumbs et répertoires .. .
+$files = array_slice($files, 2);
+$key = array_search('thumbs', $files);
+if(($key = array_search('thumbs', $files)) !== false) {
+    unset($files[$key]);
+}
+// var_dump($files);
 
-        var_dump($file);
+foreach ($files as $key => $file) {
+
+        // var_dump($key);
 
         $arr1[] = $file;
+
 
         $data = exif_read_data('images/'.$file);
         $search = array_key_exists('GPSLatitude', $data);
         // var_dump($data);
-        var_dump($data['GPSLatitude']);
-        var_dump($data['GPSLongitude']);
+        // var_dump($data['GPSLatitude']);
+        // var_dump($data['GPSLongitude']);
 
         // var_dump($search);
         // print_r(array_keys($data));
@@ -59,45 +90,47 @@ foreach ($files as $file) {
             next($data);
         }*/
 
-        // convert Exif GPS array https://stackoverflow.com/a/16437888/4264406
         $latitude = gps($data["GPSLatitude"], $data['GPSLatitudeRef']);
         $arrLat[] = $latitude;
         $longitude = gps($data["GPSLongitude"], $data['GPSLongitudeRef']);
         $arrLong[] = $longitude;
 
-        var_dump($latitude);
-        var_dump($longitude);
+        $multArray[] = array($file,$latitude,$longitude);
 
-    }
+        // var_dump($latitude);
+        // var_dump($longitude);
 
 }
 
-var_dump($arr1);
-var_dump($arrLat);
-var_dump($arrLong);
+// var_dump($arr1);
+// var_dump($multArray);
+// var_dump($arrLat);
+// var_dump($arrLong);
 
 ?>
     <div id="mapid"></div>
 
 <script type="text/javascript">
-    var latitude = "<?php echo $latitude;?>";
+    /*var latitude = "<?php echo $latitude;?>";
     var longitude = "<?php echo $longitude;?>";
     var arr1 = '<?php echo json_encode($arr1);?>';
     var arrLat = '<?php echo json_encode($arrLat);?>';
-    var arrLong = '<?php echo json_encode($arrLong);?>';
+    var arrLong = '<?php echo json_encode($arrLong);?>';*/
+    var multArray = '<?php echo json_encode($multArray);?>';
+    var data = JSON.parse(multArray);
 
     var arrLatarray = [];
 
-    for(var i in arrLat) {
+    /*for(var i in arrLat) {
         if(arrLat.hasOwnProperty(i) && !isNaN(+i)){
             arrLatarray[+i] = arrLat[i];
         }
-    }
+    }*/
 
-    console.log(arr1);
-    console.log(arrLatarray);
-    console.log(arrLat);
-    console.log(arrLong);
+    // console.log(arr1);
+    // console.log(arrLatarray);
+    // console.log(arrLat);
+    // console.log(arrLong);
 
     var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
@@ -112,18 +145,44 @@ var_dump($arrLong);
     // L.marker([51.5, -0.09]).addTo(mymap)
     //     .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 
-    for (var i = 0; i < arr1.length; i++) {
-        console.log(i);
+    // for (var i = 0; i < arr1.length; i++) {
+    //     L.marker([latitude, longitude]).addTo(mymap).bindPopup("<b>Hello world!</b><br />I am a popup.<img src='images/thumbs/image-thumb.jpg' >").openPopup();
+    // }
 
-        L.marker([latitude, longitude]).addTo(mymap)
-            .bindPopup("<b>Hello world!</b><br />I am a popup.<img src='image-thumb.jpg' >").openPopup();
+    var bounds = [];
+    var markerCarte = document.getElementsByClassName('leaflet-marker-icon');
 
+    for (var i = 0; i < data.length; i++) {
+        // console.log(data[i])
+        var latLong = [data[i][1], data[i][2]];
+        bounds.push(latLong)
+        var imgIcon = './images/thumbs/'+data[i][0];
+
+        var customIcon = L.icon({ iconUrl: imgIcon });
+
+        // console.log(imgIcon);
+        // L.marker(latLong,{iconUrl: imgIcon}).addTo(mymap).bindPopup(L.popup({autoClose:false}).setContent("<!--<b>Hello world!</b><br />I am a popup.--><img src='images/thumbs/"+(data[i][0]).replace(".jpg","")+"-thumb.jpg' width='40' height='40'>")).openPopup();
+        L.marker(latLong,{icon: customIcon,riseOnHover: true}).addTo(mymap);/*.bindPopup(L.popup({autoClose:false}).setContent("<!--<b>Hello world!</b><br />I am a popup.--><img src='images/thumbs/"+(data[i][0]).replace(".jpg","")+"-thumb.jpg' width='40' height='40'>")).openPopup();*/
+
+        // wrap img with a href element https://plainjs.com/javascript/manipulation/wrap-an-html-structure-around-an-element-28/
+        var wrapper = document.createElement('a');
+        var cheminImg = './images/'+data[i][0];
+        wrapper.setAttribute('href',cheminImg);
+        wrapper.setAttribute('target','_blank');
+        markerCarte[i].parentNode.insertBefore(wrapper, markerCarte[i]);
+        wrapper.appendChild(markerCarte[i]);
     }
+
+    // console.log(markerCarte);
+    /*for (var i = 0; i < markerCarte.length; i++) {
+    }*/
+
+    mymap.fitBounds(bounds);
 
     // L.marker([latitude, longitude]).addTo(mymap)
     //     .bindPopup("<b>Hello world!</b><br />I am a popup.<img src='image-thumb.jpg' >").openPopup();
 
-    L.circle([51.508, -0.11], 500, {
+    /*L.circle([51.508, -0.11], 500, {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5
@@ -145,7 +204,8 @@ var_dump($arrLong);
             .openOn(mymap);
     }
 
-    mymap.on('click', onMapClick);
+    mymap.on('click', onMapClick);*/
+
 
 </script>
 
